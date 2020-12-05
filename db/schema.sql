af242d2bff939a42c86e6cfd2195a5f056629713
Christopher Rojas <christopherrojas35@yahoo.com>
2020-12-04 22:02:10 -0700

    added connection for jawsDB

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,16 @@
+
 DROP DATABASE IF EXISTS burgers_db;
 
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
+-- If using Jaws DB do not use the above
 CREATE TABLE burgers
 (
-    id int NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT NOT NULL,
     burger_name varchar(255) NOT NULL,
     devoured BOOLEAN DEFAULT false NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
